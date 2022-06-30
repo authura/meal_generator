d38440e7e173ae14fb3589fbc41f3e4ff97ce13f
authura <rhoferdevs@gmail.com>
2022-06-30 13:46:08 -0400

    add new recipe

--- meal_generator.py
+++ meal_generator.py
@@ -1,6 +1,8 @@
 tuna_casserole = open('./Recipes/tuna_casserole.txt', 'r')
+lemon_chicken = open('./Recipes/lemon_chicken.txt', 'r')
+spaghetti = open('./Recipes/spaghetti.txt', 'r')
 
-with tuna_casserole as f:
+with spaghetti as f:
     f_contents = f.read()
     print(f_contents)
     f.close()
