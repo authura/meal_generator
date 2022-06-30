3b6f8ee66d56e41ec6dc090d704e9c0606375a62
authura <rhoferdevs@gmail.com>
2022-06-30 10:59:49 -0400

    stored file to read in var

--- meal_generator.py
+++ meal_generator.py
@@ -1,4 +1,6 @@
-with open('tuna_casserole.txt', 'r') as f:
+tuna_casserole = open('tuna_casserole.txt', 'r')
+
+with tuna_casserole as f:
     f_contents = f.read()
     print(f_contents)
     f.close()
