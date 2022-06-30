1156ad78391f31c1a343f9a86c1ad5db880ba6bb
authura <rhoferdevs@gmail.com>
2022-06-30 13:48:21 -0400

    store more files

--- meal_generator.py
+++ meal_generator.py
@@ -1,6 +1,9 @@
 tuna_casserole = open('./Recipes/tuna_casserole.txt', 'r')
 lemon_chicken = open('./Recipes/lemon_chicken.txt', 'r')
 spaghetti = open('./Recipes/spaghetti.txt', 'r')
+goulash = open('./Recipes/goulash.txt', 'r')
+ranch_chicken_parm = open('./Recipes/ranch_chicken_parm.txt', 'r')
+chicken_marsala = open('./Recipes/chicken_marsala.txt', 'r')
 
 with spaghetti as f:
     f_contents = f.read()
