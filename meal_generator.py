2d3a554308934a252e454927d90fde35e33c78dd
authura <rhoferdevs@gmail.com>
2022-06-30 14:01:52 -0400

    import random, shuffle files into new list and print only the first element

--- meal_generator.py
+++ meal_generator.py
@@ -1,3 +1,5 @@
+import random
+
 tuna_casserole = open('./Recipes/tuna_casserole.txt', 'r')
 lemon_chicken = open('./Recipes/lemon_chicken.txt', 'r')
 spaghetti = open('./Recipes/spaghetti.txt', 'r')
@@ -5,8 +7,14 @@ goulash = open('./Recipes/goulash.txt', 'r')
 ranch_chicken_parm = open('./Recipes/ranch_chicken_parm.txt', 'r')
 chicken_marsala = open('./Recipes/chicken_marsala.txt', 'r')
 
-with spaghetti as f:
-    f_contents = f.read()
-    print(f_contents)
-    f.close()
-    pass
+recipes = [tuna_casserole, lemon_chicken, spaghetti, goulash, 
+ranch_chicken_parm, chicken_marsala]
+
+random.shuffle(recipes)
+
+for i in recipes[:1]:
+    with i as f:
+        f_contents = f.read()
+        print(f_contents)
+        f.close()
+        pass
