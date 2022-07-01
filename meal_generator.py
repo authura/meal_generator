19fc7772e7ced7d01c9d59d8a9f8d116c503bca2
authura <rhoferdevs@gmail.com>
2022-06-30 20:17:02 -0400

    add new WIP recipes

--- meal_generator.py
+++ meal_generator.py
@@ -6,9 +6,16 @@ spaghetti = open('./Recipes/spaghetti.txt', 'r')
 goulash = open('./Recipes/goulash.txt', 'r')
 ranch_chicken_parm = open('./Recipes/ranch_chicken_parm.txt', 'r')
 chicken_marsala = open('./Recipes/chicken_marsala.txt', 'r')
+cheeseburgers = open('./Recipes/cheeseburgers.txt', 'r')
+chicken_bacon_ranch = open('./Recipes/chicken_bacon_ranch.txt', 'r')
+scooby_doo = open('./Recipes/scooby_doo.txt', 'r')
+black_bean_burger = open('./Recipes/black_bean_burger.txt', 'r')
+kielbasa = open('./Recipes/kielbasa.txt', 'r')
+lentil_soup = open('./Recipes/lentil_soup.txt', 'r')
 
 recipes = [tuna_casserole, lemon_chicken, spaghetti, goulash, 
-ranch_chicken_parm, chicken_marsala]
+ranch_chicken_parm, chicken_marsala, cheeseburgers, chicken_bacon_ranch, 
+scooby_doo, black_bean_burger, kielbasa, lentil_soup]
 
 random.shuffle(recipes)
 
